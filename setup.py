f1cd8312487dd028e50318f004928730460862be
Luiz Gustavo Hafemann <luiz.gustavo-hafemann@ubisoft.com>
2022-08-11 16:30:04 -0400

    packaging

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='slpt',
       version='1.0',
 
       python_requires='>=3',
-      packages=['slpt', 'slpt.SLPT', 'slpt.config', 'slpt.backbone', 'slpt.facedetector', 'slpt.utils'],
+      packages=['slpt', 'slpt.SLPT', 'slpt.config', 'slpt.backbone', 'slpt.facedetector', 'slpt.utils', 'slpt.dataloader'],
       include_package_data=True,
       )
